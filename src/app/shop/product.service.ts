7165570e760e3eb80fbb12cf25269cd653934a4d
unknown <tymofiimustetsov@gmail.com>
2019-09-06 21:04:31 +0300

    sep 06 BIG CHANGE

--- src/app/shop/product.service.ts
+++ src/app/shop/product.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { map } from 'rxjs/operators';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
@@ -34,6 +36,18 @@ updateProduct(productId, product) {
 delete(productId) {
   return this.db.object('products/' + productId).remove();
 }
+
+
+
+getAll2() {
+  return this.db.list('/products').snapshotChanges()
+    .pipe(
+      map(actions =>
+         actions.map(a => ({ key: a.key, ...a.payload.val()} as Product )) // here you have made something strange.
+      )
+    );
+}
+
 
 
 }
--- src/app/shop/shop/shop.component.ts
+++ src/app/shop/shop/shop.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from '../product';
+import { map } from 'rxjs/operators';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-shop',
@@ -9,31 +12,41 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
-  products$;
+  // products$;
   categories$;
   category: string;
+  prod: Product [] = [];
+  filteredProducts: Product[];
 
   constructor(
     route: ActivatedRoute,
     private db: AngularFireDatabase,
-    categoryService: CategoryService) {
+    categoryService: CategoryService,
+    productService: ProductService) {
 
     this.categories$ = categoryService.getCategories();
+
     route.queryParamMap.subscribe(params => {
-      let category = params.get('category');
+      this.category = params.get('category');
 
-
+      this.filteredProducts = (this.category) ?
+      this.prod.filter(p => p.category === this.category) :
+      this.prod;
     });
-  }
+
+
+    productService.getAll2().subscribe(prod => this.prod = prod);
+
+
+    }
+
+
+
 
   ngOnInit() {
 
     // мы используем valueChanges() - возвращает массив, а snapshotChanges() - возвращает observable
-
-    this.products$ = this.db.list('products').valueChanges();
-
-
-
+    // this.products$ = this.db.list('products').valueChanges();
     }
 
 }
